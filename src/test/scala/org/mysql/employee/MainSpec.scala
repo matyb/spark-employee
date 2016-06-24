cfa084efcaa297f809108d342b167363e31e0fa4
matyb <mathewkbentley@gmail.com>
2016-06-23 23:03:17 -0400

    Adding travis configuration

--- src/test/scala/org/mysql/employee/MainSpec.scala
+++ src/test/scala/org/mysql/employee/MainSpec.scala
@@ -71,18 +71,18 @@ class MainSpec extends FunSpec with SharedSparkContext with Matchers {
   describe("Constructing Department Employee Relationship RDD") {
 
     it("reads a department employee relationsip if line starts with 'INSERT'") {
-      becomes(Array("INSERT INTO `dept_emp` VALUES (10001,'d005','1986-06-26','9999-01-01'),"), DepartmentEmployee, 
-          Array(DepartmentEmployee("10001","d005",sdf.parse("1986-06-26"),sdf.parse("9999-01-01"))))
+      becomes(Array("INSERT INTO `dept_emp` VALUES (10001,'d005','1986-06-26','9999-01-01'),"), DepartmentEmployee,
+        Array(DepartmentEmployee("10001", "d005", sdf.parse("1986-06-26"), sdf.parse("9999-01-01"))))
     }
 
     it("reads a department employee relationsip if line starts with '('") {
-      becomes(Array("(10001,'d005','1986-06-26','9999-01-01'),"), DepartmentEmployee, 
-          Array(DepartmentEmployee("10001","d005",sdf.parse("1986-06-26"),sdf.parse("9999-01-01"))))
+      becomes(Array("(10001,'d005','1986-06-26','9999-01-01'),"), DepartmentEmployee,
+        Array(DepartmentEmployee("10001", "d005", sdf.parse("1986-06-26"), sdf.parse("9999-01-01"))))
     }
 
     it("reads a department employee relationsip if line ends with ');' instead of '),'") {
-      becomes(Array("(10001,'d005','1986-06-26','9999-01-01');"), DepartmentEmployee, 
-          Array(DepartmentEmployee("10001","d005",sdf.parse("1986-06-26"),sdf.parse("9999-01-01"))))
+      becomes(Array("(10001,'d005','1986-06-26','9999-01-01');"), DepartmentEmployee,
+        Array(DepartmentEmployee("10001", "d005", sdf.parse("1986-06-26"), sdf.parse("9999-01-01"))))
     }
 
   }
@@ -94,32 +94,32 @@ class MainSpec extends FunSpec with SharedSparkContext with Matchers {
     }
 
     it("reads a department manager relationsip if line starts with '('") {
-      becomes(Array("(110022,'d004','1985-01-01','1991-10-01'),"), DepartmentManager, Array(DepartmentManager("110022","d004",sdf.parse("1985-01-01"),sdf.parse("1991-10-01"))))
+      becomes(Array("(110022,'d004','1985-01-01','1991-10-01'),"), DepartmentManager, Array(DepartmentManager("110022", "d004", sdf.parse("1985-01-01"), sdf.parse("1991-10-01"))))
     }
 
     it("reads a department employee relationsip if line ends with ');' instead of '),'") {
-      becomes(Array("(110022,'d004','1985-01-01','1991-10-01');"), DepartmentManager, Array(DepartmentManager("110022","d004",sdf.parse("1985-01-01"),sdf.parse("1991-10-01"))))
+      becomes(Array("(110022,'d004','1985-01-01','1991-10-01');"), DepartmentManager, Array(DepartmentManager("110022", "d004", sdf.parse("1985-01-01"), sdf.parse("1991-10-01"))))
     }
 
   }
-  
+
   describe("Constructing Employee Title RDD") {
 
     it("reads an employee title if line starts with 'INSERT'") {
-      becomes(Array("INSERT INTO `titles` VALUES (10001,'Senior Engineer','1986-06-26','9999-01-01'),"), EmployeeTitle, 
-          Array(EmployeeTitle("10001","Senior Engineer",sdf.parse("1986-06-26"),sdf.parse("9999-01-01"))))
+      becomes(Array("INSERT INTO `titles` VALUES (10001,'Senior Engineer','1986-06-26','9999-01-01'),"), EmployeeTitle,
+        Array(EmployeeTitle("10001", "Senior Engineer", sdf.parse("1986-06-26"), sdf.parse("9999-01-01"))))
     }
 
     it("reads an employee title if line starts with '('") {
-      becomes(Array("(10001,'Senior Engineer','1986-06-26','9999-01-01'),"), EmployeeTitle, 
-          Array(EmployeeTitle("10001","Senior Engineer",sdf.parse("1986-06-26"),sdf.parse("9999-01-01"))))
+      becomes(Array("(10001,'Senior Engineer','1986-06-26','9999-01-01'),"), EmployeeTitle,
+        Array(EmployeeTitle("10001", "Senior Engineer", sdf.parse("1986-06-26"), sdf.parse("9999-01-01"))))
     }
-    
+
     it("reads an employee title if line ends with ');'") {
-      becomes(Array("(10001,'Senior Engineer','1986-06-26','9999-01-01');"), EmployeeTitle, 
-          Array(EmployeeTitle("10001","Senior Engineer",sdf.parse("1986-06-26"),sdf.parse("9999-01-01"))))
+      becomes(Array("(10001,'Senior Engineer','1986-06-26','9999-01-01');"), EmployeeTitle,
+        Array(EmployeeTitle("10001", "Senior Engineer", sdf.parse("1986-06-26"), sdf.parse("9999-01-01"))))
     }
-    
+
   }
 
   def readRecords(fileName: String) = {
@@ -128,101 +128,104 @@ class MainSpec extends FunSpec with SharedSparkContext with Matchers {
     text.split("\\r?\\n")
   }
 
-  describe("Can construct RDDs from actual MySQL files") {
+  System.setProperty("integration", "true")
+  if (!"true".equals(System.getProperty("integration"))) {
+    describe("Can construct RDDs from actual MySQL files") {
 
-    def loadEmployees(): (Array[String], RDD[EmployeeDemographic], List[EmployeeDemographic]) = {
-      val records = readRecords("load_employees.dump")
-      val rdd = Main.parse(sc.parallelize(records), EmployeeDemographic)
-      (records, rdd, rdd.collect().toList)
+      def loadEmployees(): (Array[String], RDD[EmployeeDemographic], List[EmployeeDemographic]) = {
+        val records = readRecords("load_employees.dump")
+        val rdd = Main.parse(sc.parallelize(records), EmployeeDemographic)
+        (records, rdd, rdd.collect().toList)
+      }
+
+      it("creates the same number of employee records, first and last are equal to those in file") {
+        val (records, employeesRdd, employees) = loadEmployees()
+        records.length should equal(employees.length)
+        employees(0) should equal(EmployeeDemographic("10001", sdf.parse("1953-09-02"), "Georgi", "Facello", Gender withName ("M"), sdf.parse("1986-06-26")))
+        employees.last should equal(EmployeeDemographic("499999", sdf.parse("1958-05-01"), "Sachin", "Tsukuda", Gender withName "M", sdf.parse("1997-11-30")))
+      }
+
+      def loadDepartments(): (Array[String], RDD[Department], List[Department]) = {
+        val records = readRecords("load_departments.dump")
+        val departments = Main.parse(sc.parallelize(records), Department)
+        (records, departments, departments.collect().toList)
+      }
+
+      it("creates the same number of department records, first and last are equal to those in file") {
+        val (records, departmentsRdd, departments) = loadDepartments()
+        records.length - 1 should equal(departments.length) // INSERT is on row by itself and is dropped
+        departments(0) should equal(Department("d001", "Marketing"))
+        departments.last should equal(Department("d009", "Customer Service"))
+      }
+
+      def loadDepartmentEmployees(): (Array[String], RDD[DepartmentEmployee], List[DepartmentEmployee]) = {
+        val records = readRecords("load_dept_emp.dump")
+        val departmentEmployees = Main.parse(sc.parallelize(records), DepartmentEmployee)
+        (records, departmentEmployees, departmentEmployees.collect().toList)
+      }
+
+      it("creates the same number of department_employee records, first and last are equal to those in file") {
+        val (records, departmentEmployeesRdd, departmentEmployees) = loadDepartmentEmployees()
+
+        val cachedDepartmentEmployees = departmentEmployeesRdd.cache()
+
+        cachedDepartmentEmployees.count() should equal(records.length)
+
+        val firstDeptEmp = List(DepartmentEmployee.convert(Array("10001", "d005", "1986-06-26", "9999-01-01")))
+
+        cachedDepartmentEmployees.filter { _.employeeId == "10001" }.collect() should equal(firstDeptEmp)
+
+        val lastDeptEmp = List(DepartmentEmployee.convert(Array("499999", "d004", "1997-11-30", "9999-01-01")))
+
+        cachedDepartmentEmployees.filter { _.employeeId == "499999" }.collect() should equal(lastDeptEmp)
+      }
+
+      def loadDepartmentManagers(): (Array[String], RDD[DepartmentManager], List[DepartmentManager]) = {
+        val records = readRecords("load_dept_manager.dump")
+        val departmentManagers = Main.parse(sc.parallelize(records), DepartmentManager)
+        (records, departmentManagers, departmentManagers.collect().toList)
+      }
+
+      it("creates the same number of department_manager records, first and last are equal to those in file") {
+        val (records, departmentManagersRdd, departmentManagers) = loadDepartmentManagers()
+
+        val cachedDepartmentManagers = departmentManagersRdd.cache()
+
+        cachedDepartmentManagers.count() should equal(records.filter { row => !row.isEmpty() && !row.trim().endsWith("VALUES") }.length)
+
+        val firstDeptMgr = List(DepartmentManager.convert(Array("110022", "d001", "1985-01-01", "1991-10-01")))
+
+        cachedDepartmentManagers.filter { _.employeeId == "110022" }.collect() should equal(firstDeptMgr)
+
+        val lastDeptMgr = List(DepartmentManager.convert(Array("111939", "d009", "1996-01-03", "9999-01-01")))
+
+        cachedDepartmentManagers.filter { _.employeeId == "111939" }.collect() should equal(lastDeptMgr)
+      }
+
+      def loadEmployeeTitles(): (Array[String], RDD[EmployeeTitle], List[EmployeeTitle]) = {
+        val records = readRecords("load_titles.dump")
+        val employeeTitles = Main.parse(sc.parallelize(records), EmployeeTitle)
+        (records, employeeTitles, employeeTitles.collect().toList)
+      }
+
+      it("creates the same number of employee_title records, first and last are equal to those in file") {
+        val (records, employeeTitlesRdd, employeeTitles) = loadEmployeeTitles()
+
+        val cachedEmployeeTitles = employeeTitlesRdd.cache()
+
+        cachedEmployeeTitles.count() should equal(records.length)
+
+        val firstEmpTitle = List(EmployeeTitle.convert(Array("10001", "Senior Engineer", "1986-06-26", "9999-01-01")))
+
+        cachedEmployeeTitles.filter { _.employeeId == "10001" }.collect() should equal(firstEmpTitle)
+
+        val lastEmpTitle = List(EmployeeTitle.convert(Array("499999", "Engineer", "1997-11-30", "9999-01-01")))
+
+        cachedEmployeeTitles.filter { _.employeeId == "499999" }.collect() should equal(lastEmpTitle)
+      }
+      
     }
-
-    it("creates the same number of employee records, first and last are equal to those in file") {
-      val (records, employeesRdd, employees) = loadEmployees()
-      records.length should equal(employees.length)
-      employees(0) should equal(EmployeeDemographic("10001", sdf.parse("1953-09-02"), "Georgi", "Facello", Gender withName ("M"), sdf.parse("1986-06-26")))
-      employees.last should equal(EmployeeDemographic("499999", sdf.parse("1958-05-01"), "Sachin", "Tsukuda", Gender withName "M", sdf.parse("1997-11-30")))
-    }
-
-    def loadDepartments(): (Array[String], RDD[Department], List[Department]) = {
-      val records = readRecords("load_departments.dump")
-      val departments = Main.parse(sc.parallelize(records), Department)
-      (records, departments, departments.collect().toList)
-    }
-
-    it("creates the same number of department records, first and last are equal to those in file") {
-      val (records, departmentsRdd, departments) = loadDepartments()
-      records.length - 1 should equal(departments.length) // INSERT is on row by itself and is dropped
-      departments(0) should equal(Department("d001", "Marketing"))
-      departments.last should equal(Department("d009", "Customer Service"))
-    }
-
-    def loadDepartmentEmployees(): (Array[String], RDD[DepartmentEmployee], List[DepartmentEmployee]) = {
-      val records = readRecords("load_dept_emp.dump")
-      val departmentEmployees = Main.parse(sc.parallelize(records), DepartmentEmployee)
-      (records, departmentEmployees, departmentEmployees.collect().toList)
-    }
-    
-    it("creates the same number of department_employee records, first and last are equal to those in file") {
-    	val (records, departmentEmployeesRdd, departmentEmployees) = loadDepartmentEmployees()
-    			
-    			val cachedDepartmentEmployees = departmentEmployeesRdd.cache()
-    			
-    			cachedDepartmentEmployees.count() should equal(records.length)
-    			
-    			val firstDeptEmp = List(DepartmentEmployee.convert(Array("10001", "d005", "1986-06-26", "9999-01-01")))
-    			
-    			cachedDepartmentEmployees.filter { _.employeeId == "10001" }.collect() should equal(firstDeptEmp)
-    			
-    			val lastDeptEmp = List(DepartmentEmployee.convert(Array("499999", "d004", "1997-11-30", "9999-01-01")))
-    			
-    			cachedDepartmentEmployees.filter { _.employeeId == "499999" }.collect() should equal(lastDeptEmp)
-    }
-
-    def loadDepartmentManagers(): (Array[String], RDD[DepartmentManager], List[DepartmentManager]) = {
-      val records = readRecords("load_dept_manager.dump")
-      val departmentManagers = Main.parse(sc.parallelize(records), DepartmentManager)
-      (records, departmentManagers, departmentManagers.collect().toList)
-    }
-
-    it("creates the same number of department_manager records, first and last are equal to those in file") {
-      val (records, departmentManagersRdd, departmentManagers) = loadDepartmentManagers()
-
-      val cachedDepartmentManagers = departmentManagersRdd.cache()
-
-      cachedDepartmentManagers.count() should equal(records.filter { row => !row.isEmpty() && !row.trim().endsWith("VALUES") }.length)
-
-      val firstDeptMgr = List(DepartmentManager.convert(Array("110022", "d001", "1985-01-01", "1991-10-01")))
-
-      cachedDepartmentManagers.filter { _.employeeId == "110022" }.collect() should equal(firstDeptMgr)
-
-      val lastDeptMgr = List(DepartmentManager.convert(Array("111939", "d009", "1996-01-03", "9999-01-01")))
-
-      cachedDepartmentManagers.filter { _.employeeId == "111939" }.collect() should equal(lastDeptMgr)
-    }
-
-    def loadEmployeeTitles(): (Array[String], RDD[EmployeeTitle], List[EmployeeTitle]) = {
-      val records = readRecords("load_titles.dump")
-      val employeeTitles = Main.parse(sc.parallelize(records), EmployeeTitle)
-      (records, employeeTitles, employeeTitles.collect().toList)
-    }
-
-    it("creates the same number of employee_title records, first and last are equal to those in file") {
-      val (records, employeeTitlesRdd, employeeTitles) = loadEmployeeTitles()
-
-      val cachedEmployeeTitles = employeeTitlesRdd.cache()
-
-      cachedEmployeeTitles.count() should equal(records.length)
-
-      val firstEmpTitle = List(EmployeeTitle.convert(Array("10001", "Senior Engineer", "1986-06-26", "9999-01-01")))
-
-      cachedEmployeeTitles.filter { _.employeeId == "10001" }.collect() should equal(firstEmpTitle)
-
-      val lastEmpTitle = List(EmployeeTitle.convert(Array("499999", "Engineer", "1997-11-30", "9999-01-01")))
-
-      cachedEmployeeTitles.filter { _.employeeId == "499999" }.collect() should equal(lastEmpTitle)
-    }
-
     
   }
-  
+
 }
