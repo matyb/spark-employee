5ab866b3e1b7f4b67bde5d16bb5c4d27c3061cde
matyb <mathewkbentley@gmail.com>
2016-06-23 23:57:36 -0400

    adding default tmp.folder temporarily

--- src/test/scala/org/mysql/employee/utils/FileUtilsSpec.scala
+++ src/test/scala/org/mysql/employee/utils/FileUtilsSpec.scala
@@ -1,15 +1,15 @@
 package org.mysql.employee.utils
 
-import org.scalatest.FlatSpec
 import java.io.File
+
 import org.scalatest.BeforeAndAfterEach
+import org.scalatest.FlatSpec
 
 class FileUtilsSpec extends FlatSpec with BeforeAndAfterEach {
 
-  val tmpFolder = System.getProperty("tmp.folder")
+  val tmpFolder = if (System.getProperty("tmp.folder") == null) System.getProperty("tmp.folder") else "/tmp/spark-employee"
 
   override def beforeEach() {
-    if (tmpFolder == null) throw new IllegalStateException("property 'tmp.folder' is required") 
     new File(tmpFolder).delete()
     assert(!new File(tmpFolder).exists())
   }
