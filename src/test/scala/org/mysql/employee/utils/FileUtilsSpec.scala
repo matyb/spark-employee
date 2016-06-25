7e789500309e7716f19d9cf7f385cac343960b90
matyb <mathewkbentley@gmail.com>
2016-06-24 21:05:12 -0400

    trying travis again..

--- src/test/scala/org/mysql/employee/utils/FileUtilsSpec.scala
+++ src/test/scala/org/mysql/employee/utils/FileUtilsSpec.scala
@@ -1,28 +1,24 @@
 package org.mysql.employee.utils
 
-import java.io.File
-
-import org.scalatest.BeforeAndAfterEach
 import org.scalatest.FlatSpec
+import java.io.File
+import org.scalatest.BeforeAndAfterEach
 
 class FileUtilsSpec extends FlatSpec with BeforeAndAfterEach {
 
-  val tmpFolder = if (System.getProperty("tmp.folder") != null) System.getProperty("tmp.folder") else "\\tmp\\spark-employee" 
+  val tmpFolder = if (System.getProperty("tmp.folder") != null) System.getProperty("tmp.folder") else "\\tmp\\spark-employee"
 
   override def beforeEach() {
-    if(false){
-      new File(tmpFolder).delete()
-      assert(!new File(tmpFolder).exists())
-    }
+    if (tmpFolder == null) throw new IllegalStateException("property 'tmp.folder' is required") 
+    new File(tmpFolder).delete()
+    assert(!new File(tmpFolder).exists())
   }
 
   it should "remove folders" in {
-    if(false){
-      new File(tmpFolder).createNewFile()
-      assert(new File(tmpFolder).exists())
-      FileUtils.rmFolder(tmpFolder)
-      assert(!new File(tmpFolder).exists())
-    }
+    new File(tmpFolder).createNewFile()
+    assert(new File(tmpFolder).exists())
+    FileUtils.rmFolder(tmpFolder)
+    assert(!new File(tmpFolder).exists())
   }
 
 }
